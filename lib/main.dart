464ebe8683c73811b4f552a29644df70738576df
ahmad rafi wirana <ahmad.rafi02@ui.ac.id>
2022-03-10 19:41:37 +0700

    adding splash screen app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
  import 'package:flutter/material.dart';
+ import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:simple_todo/screens/tabs_screen.dart';
 import 'package:simple_todo/screens/home_page.dart';
 import 'package:simple_todo/screens/profile_page.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
